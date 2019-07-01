248d84ba99554ae7bfd20d74899e6bbecbd6292a
Santhosh <santhosh_nampally@epam.com>
2019-07-01 18:32:59 +0530

    added Text Span styles

--- flutter_sample_text/lib/main.dart
+++ flutter_sample_text/lib/main.dart
@@ -4,11 +4,11 @@ void main() {
   runApp(
       new Center(
           child: new Text.rich(
-            TextSpan(text: "Santhosh ",style: TextStyle(color: Colors.blue),
+            TextSpan(text: "Santhosh ",style: TextStyle(color: Colors.blue,fontSize: 30, fontWeight: FontWeight.lerp(FontWeight.w100, FontWeight.w900, 1)) ,
                 children: [
-                  TextSpan(text: "Super cool ",style: TextStyle(color:  Colors.amber)),
-                  TextSpan(text: "Super cool ",style: TextStyle(color:  Colors.deepOrangeAccent)),
-                  TextSpan(text: "Super cool ",style: TextStyle(color:  Colors.cyan)),
+                  TextSpan(text: "Super cool ",style: TextStyle(color:  Colors.amber, fontSize: 10)),
+                  TextSpan(text: "Super cool ",style: TextStyle(color:  Colors.deepOrangeAccent, fontSize: 15)),
+                  TextSpan(text: "Super cool ",style: TextStyle(color:  Colors.cyan, fontWeight: FontWeight.bold)),
                 ]),
             textDirection: TextDirection.rtl,
             textAlign: TextAlign.left,
